36b9527a9848370e80571ff8d696fb716cb28f40
xiaoyao9184 <xiaoyao9184@gmail.com>
2018-09-21 15:25:16 +0800

    Fix multiple connections do not enter the connection pool

--- zuul-localtunnel/src/main/java/com/xy/spring/cloud/zuul/tunnel/apache/AutoReleaseConnectionSocketApplicationListener.java
+++ zuul-localtunnel/src/main/java/com/xy/spring/cloud/zuul/tunnel/apache/AutoReleaseConnectionSocketApplicationListener.java
@@ -1,19 +1,29 @@
 package com.xy.spring.cloud.zuul.tunnel.apache;
 
 import com.xy.spring.cloud.zuul.tunnel.localtunnel.ClientManager;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.apache.http.HttpClientConnection;
 import org.apache.http.conn.ConnectionRequest;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.pool.AbstractConnPool;
 import org.apache.http.protocol.BasicHttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.util.ReflectionUtils;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 /**
  * Created by xiaoyao9184 on 2018/9/10.
@@ -26,19 +36,65 @@ public class AutoReleaseConnectionSocketApplicationListener
     private HttpClientConnectionManager manager;
     private HttpRouteLocator locator;
 
+    private Object pool;
+    private static Object notExistState = new Object();
+
     public AutoReleaseConnectionSocketApplicationListener(
             HttpClientConnectionManager manager,
             HttpRouteLocator locator){
         this.manager = manager;
         this.locator = locator;
+
+        //connection pool
+        Field field = ReflectionUtils
+                .findField(PoolingHttpClientConnectionManager.class, "pool");
+        ReflectionUtils.makeAccessible(field);
+        this.pool = ReflectionUtils.getField(field, this.manager);
     }
 
+    /**
+     * init proxy route pool by replace it
+     * @param route HttpRoute
+     */
+    private void initProxyPool(HttpRoute route){
+        //get route pool map
+        Field routeToPoolField = ReflectionUtils
+                .findField(AbstractConnPool.class, "routeToPool");
+        ReflectionUtils.makeAccessible(routeToPoolField);
+        @SuppressWarnings("unchecked")
+        Map<HttpRoute,Object> routeToPool = (Map<HttpRoute, Object>) ReflectionUtils.getField(routeToPoolField, this.pool);
+
+        //init normal pool
+        Method method = ReflectionUtils
+                .findMethod(AbstractConnPool.class, "getPool", Object.class);
+        ReflectionUtils.makeAccessible(method);
+        Object routeSpecificPool = ReflectionUtils.invokeMethod(method, this.pool, route);
+
+        //check is proxy pool
+        Method[] methods = routeSpecificPool.getClass().getDeclaredMethods();
+        boolean isProxy = Stream.of(methods)
+                .anyMatch(m -> m.getName().endsWith("getTargetSource"));
+        if(isProxy){
+            return;
+        }
+
+        //create proxy pool
+        ProxyFactory factory = new ProxyFactory();
+        factory.setTarget(routeSpecificPool);
+        factory.addAdvice(new RouteSpecificPoolGetPoolMethodInterceptor());
+        Object proxyRouteSpecificPool = factory.getProxy();
+
+        //replace normal pool with proxy pool
+        routeToPool.replace(route,routeSpecificPool, proxyRouteSpecificPool);
+    }
 
     public void release(String id, HttpRoute route) throws InterruptedException, ExecutionException, IOException {
         BasicHttpContext context = new BasicHttpContext();
         context.setAttribute("tunnel.id", id);
 
-        ConnectionRequest connectionRequest = manager.requestConnection(route, null);
+        initProxyPool(route);
+
+        ConnectionRequest connectionRequest = manager.requestConnection(route, notExistState);
         //lease
         HttpClientConnection connection = connectionRequest.get(5, TimeUnit.MICROSECONDS);
         manager.connect(
@@ -111,6 +167,24 @@ public class AutoReleaseConnectionSocketApplicationListener
     public interface HttpRouteLocator {
 
         HttpRoute locate(String id);
+
+    }
+
+    /**
+     * Support get not exist state entry(null entry)
+     * @see org.apache.http.pool.RouteSpecificPool#getFree(Object)
+     */
+    private static class RouteSpecificPoolGetPoolMethodInterceptor implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            if (invocation.getMethod().getName().equals("getFree")
+                    && invocation.getArguments().length == 1
+                    && notExistState.equals(invocation.getArguments()[0])) {
+                return null;
+            }
+            return invocation.proceed();
+        }
 
     }
 }
